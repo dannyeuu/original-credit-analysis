db220bc741fbefcdfe9f341ec3dfca4d6f3213e0
Daniel Lopes <daniel@daniellopes.com.br>
2017-12-03 01:43:16 -0200

    Add Social

--- config.py
+++ config.py
@@ -11,9 +11,11 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = b'\xcagC\xac\x0e]lTY2\xd2\x9fS\xa2[#\xcfA\x0c6\x9c|\xcbs\\\xb6\xd3\xa1Q/\x8b\xd1F\x8f.se\xee\x91!\xfd\xb3\xa7\xe79\xb4\x0e\xc9'
+    
     # Database
     SQLALCHEMY_DATABASE_URI = 'sqlite:///db/test.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     # Original Integration
     ORIGINAL_API_URL = 'https://sandbox.original.com.br'
     ORIGINAL_AUTH_URL = 'https://sb-autenticacao-api.original.com.br'
@@ -22,6 +24,16 @@ class Config(object):
     ORIGINAL_SECRET_KEY = 'dd385cd0b59c013560400050569a7fac'
     ORIGINAL_ACCESS_TOKEN = 'Bearer MzA0MThkOTAtZDdiZS0xMWU3LWJjNTEtMDA1MDU2OWE3MzA1OmV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUowZVhCbElqb2lUMEYxZEdnaUxDSnBZWFFpT2pFMU1USXlOVGszTnpnc0ltVjRjQ0k2TVRVeE1qWTVNVGMzT0N3aVlYVmtJam9pTldJMFpqZG1PR1lpTENKcGMzTWlPaUphZFhBdWJXVWdSMkYwWlhkaGVTSXNJbk4xWWlJNklqTXdOREU0WkRrd0xXUTNZbVV0TVRGbE55MWlZelV4TFRBd05UQTFOamxoTnpNd05TSXNJbXAwYVNJNklqUXdOemd4T0dFd0xXUTNZbVV0TVRGbE55MWhOMk13TFdNeE56WXlaRFl3TlRRME5pSjkuc0lrVmFrby15WnZWNDR5Q1cwem5wTVdLM1pueFhJUGs3RlZVV2tuQmVXWQ=='
 
+    # Foursquare Integration
+    SOCIAL_FOURSQUARE = {
+        'consumer_key': 'VMB5XEAGSGGDCPIQ3KUWW5CMV4T0LPY3XUX2RPNNC14AC1NT',
+        'consumer_secret': 'QPEREZMKSIIT3NBTMVMCZOSOA4EAOMSQHWLCDPF0UN4W2F34'
+    }
+    # Faccebook Integration
+    SOCIAL_FACEBOOK = {
+        'consumer_key': '135017777116915',
+        'consumer_secret': '4d92534b11f3b6227561494b3c2a943f'
+    }
 
 class ProductionConfig(Config):
     DEBUG = False
