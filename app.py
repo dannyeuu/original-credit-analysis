ba3d521e43e37202100ae6b2a81660b111d1dbaf
Daniel Lopes <daniel@daniellopes.com.br>
2017-12-03 00:44:11 -0200

    Hide config

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import os
 import json
 import requests
 from flask import Flask, request
+from flask import send_from_directory
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def hello():
